0a5fce78f02da9c4d2f75b9020be84aeaa91d569
rajakumbagiri <kumbagiri.raja75.com>
2025-07-17 23:46:26 +0530

    Backend setup

--- backend.sh
+++ backend.sh
@@ -62,7 +62,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
 #check your repo and path
-cp ec2-user@ip-172-31-93-97 ~/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
